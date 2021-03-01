a2568333844bf3e5c4f364083bda333b24af2190
青椒 <zhangzhiwei@uvw.org>
2021-03-01 15:57:06 +0800

    早安英语

--- routes/base/daily.ts
+++ routes/base/daily.ts
@@ -9,7 +9,11 @@ module.exports = {
    */
   async ['/list']({ req, res, request, cheerio }) {
     const { id } = req.query
-    const html = await request.send(`http://www.kekenet.com/kouyu/primary/chuji/${id ? `List_${id}.shtml` : ''}`)
+    const html = await request.send(
+      `http://www.kekenet.com/kouyu/primary/chuji/${
+        id ? `List_${id}.shtml` : ''
+      }`
+    )
     const $ = cheerio.load(html)
     const regx = /(.*?)\.shtml/
     const result = $('#menu-list li')
@@ -75,26 +79,79 @@ module.exports = {
    */
   async ['/news_list']({ req, res, request, cheerio }) {
     const { id } = req.query
-    const html = await request.send(`http://www.kekenet.com/Article/16001/List_${id}.shtml`)
+    const html = await request.send(
+      `http://www.kekenet.com/Article/16001/List_${id}.shtml`
+    )
     const $ = cheerio.load(html)
     const regx = /http:\/\/www.kekenet.com\/Article\/(.*?)\.shtml/
-    const result = $('#menu-list li').toArray().map((item: any) => {
-      const cover = $(item).find('a').eq(0).find('img').attr('src')
-      const id = $(item).find('h2').find('a').eq(1).attr('href').match(regx)[1]
-      const title = $(item).find('h2').find('a').eq(1).text().trim().split(':')
-      const date = $(item).find('p').text().trim().match(/(.{4})-(.{2})-(.{2})/)[0]
-      return {
-        cover,
-        id,
-        title,
-        date,
-      }
-    })
+    const result = $('#menu-list li')
+      .toArray()
+      .map((item: any) => {
+        const cover = $(item).find('a').eq(0).find('img').attr('src')
+        const id = $(item)
+          .find('h2')
+          .find('a')
+          .eq(1)
+          .attr('href')
+          .match(regx)[1]
+        const title = $(item)
+          .find('h2')
+          .find('a')
+          .eq(1)
+          .text()
+          .trim()
+          .split(':')
+        const date = $(item)
+          .find('p')
+          .text()
+          .trim()
+          .match(/(.{4})-(.{2})-(.{2})/)[0]
+        return {
+          cover,
+          id,
+          title,
+          date,
+        }
+      })
 
     res.send({
       code: 200,
       data: {
         result,
+      },
+    })
+  },
+
+  async ['/zaoan_list']({ req, res, request, cheerio }) {
+    const { id } = req.query
+    const html = await request.send(
+      `http://www.kekenet.com/Article/16252/List_${id}.shtml`
+    )
+    const $ = cheerio.load(html)
+    const regx = /http:\/\/www.kekenet.com\/Article\/(.*?)\.shtml/
+    const result = $('#menu-list li')
+      .toArray()
+      .map((item: any) => {
+        const cover = $(item).find('a').eq(0).find('img').attr('src')
+        const id = $(item).find('h2').find('a').attr('href').match(regx)[1]
+        const title = $(item).find('h2').find('a').text().trim().split(':')
+        const date = $(item)
+          .find('p')
+          .text()
+          .trim()
+          .match(/(.{4})-(.{2})-(.{2})/)[0]
+        return {
+          cover,
+          id,
+          title,
+          date,
+        }
+      })
+
+    res.send({
+      code: 200,
+      data: {
+        result: result.reverse(),
       },
     })
   },
@@ -108,19 +165,35 @@ module.exports = {
    */
   async ['/news_detail']({ req, res, request, cheerio }) {
     const { id } = req.query
-    const html = await request.send(`http://www.kekenet.com/Article/${id}.shtml`)
+    const html = await request.send(
+      `http://www.kekenet.com/Article/${id}.shtml`
+    )
     const $ = cheerio.load(html)
     const title = $('#nrtitle').text().trim()
-    const content = $('#article .info-qh').html()
-    const htmlmp3 = await request.send(`http://www.kekenet.com/mp3/${id}.shtml`)
-    const $1 = cheerio.load(htmlmp3)
-    const audio = $1('.lastPage_left table').find('a').eq(1).attr('href')
+    const regx = /http:\/\/www.kekenet.com\/Article\/(.*?)\.shtml/g
+    const content = $('#article')
+      .html()
+      .replace(regx, '')
+      .replace(/#7030a0/g, '#003A29')
+    let audio = ''
+    if ($('#mp3_fileurl')) {
+      audio = $('#mp3_fileurl').attr('src')
+    } else {
+      const htmlmp3 = await request.send(`http://www.kekenet.com/mp3/${id}.shtml`)
+      const $1 = cheerio.load(htmlmp3)
+      audio = $1('.lastPage_left table').find('a').eq(1).attr('href')
+    }
+
     res.send({
       code: 200,
       data: {
         title,
-        content: StringUtil.formatUnicode(content).replace('译文属可可英语原创，未经允许，不得转载。', '').replace('译文属可可原创，仅供学习交流使用，未经许可请勿转载。', '').replace('来源:可可英语', '').replace('可可', 'Daily每日'),
-        audio
+        content: StringUtil.formatUnicode(content)
+          .replace('译文属可可英语原创，未经允许，不得转载。', '')
+          .replace('译文属可可原创，仅供学习交流使用，未经许可请勿转载。', '')
+          .replace('来源:可可英语', '')
+          .replace('可可', 'Daily每日'),
+        audio,
       },
     })
   },
@@ -134,21 +207,47 @@ module.exports = {
    */
   async ['/textbook_list']({ req, res, request, cheerio }) {
     const { id } = req.query
-    const html = await request.send(`http://www.kekenet.com/menu/13216/${id == 6 ? '' : `List_${id}.shtml`}`)
+    const html = await request.send(
+      `http://www.kekenet.com/menu/13216/${id == 6 ? '' : `List_${id}.shtml`}`
+    )
     const $ = cheerio.load(html)
     const regx = /http:\/\/www.kekenet.com\/menu\/(.*?)\.shtml/
-    const result = $('#menu-list li').toArray().map((item: any) => {
-      const id = $(item).find('h2').find('a').eq(1).attr('href').match(regx)[1]
-      const title = $(item).find('h2').find('a').eq(1).text().trim().replace('(MP3+中英字幕)', '').split(':')
-      const date = $(item).find('p').text().trim().match(/(.{4})-(.{2})-(.{2})/)[0]
-      const index = $(item).find('h2').find('a').eq(1).text().trim().match(/第(.*?)期/)[1]
-      return {
-        index,
-        id,
-        title,
-        date,
-      }
-    })
+    const result = $('#menu-list li')
+      .toArray()
+      .map((item: any) => {
+        const id = $(item)
+          .find('h2')
+          .find('a')
+          .eq(1)
+          .attr('href')
+          .match(regx)[1]
+        const title = $(item)
+          .find('h2')
+          .find('a')
+          .eq(1)
+          .text()
+          .trim()
+          .replace('(MP3+中英字幕)', '')
+          .split(':')
+        const date = $(item)
+          .find('p')
+          .text()
+          .trim()
+          .match(/(.{4})-(.{2})-(.{2})/)[0]
+        const index = $(item)
+          .find('h2')
+          .find('a')
+          .eq(1)
+          .text()
+          .trim()
+          .match(/第(.*?)期/)[1]
+        return {
+          index,
+          id,
+          title,
+          date,
+        }
+      })
     res.send({
       code: 200,
       data: {
@@ -177,8 +276,13 @@ module.exports = {
       code: 200,
       data: {
         title,
-        content: StringUtil.formatUnicode(content).replace('译文属可可英语原创，未经允许，不得转载。', '').replace('译文属可可原创，仅供学习交流使用，未经许可请勿转载。', '').replace(/来源:可可英语/g, '').replace(/可可/g, 'Daily每日').replace(/&apos/g, '’'),
-        audio
+        content: StringUtil.formatUnicode(content)
+          .replace('译文属可可英语原创，未经允许，不得转载。', '')
+          .replace('译文属可可原创，仅供学习交流使用，未经许可请勿转载。', '')
+          .replace(/来源:可可英语/g, '')
+          .replace(/可可/g, 'Daily每日')
+          .replace(/&apos/g, '’'),
+        audio,
       },
     })
   },
@@ -192,21 +296,42 @@ module.exports = {
    */
   async ['/renaibook_list']({ req, res, request, cheerio }) {
     const { id, v, max } = req.query
-    const html = await request.send(`http://www.kekenet.com/chuzhong/${v}/${id == max ? '' : `List_${id}.shtml`}`)
+    const html = await request.send(
+      `http://www.kekenet.com/chuzhong/${v}/${
+        id == max ? '' : `List_${id}.shtml`
+      }`
+    )
     const $ = cheerio.load(html)
     const regx = /http:\/\/www.kekenet.com\/chuzhong\/(.*?)\.shtml/
-    const result = $('#menu-list li').toArray().map((item: any) => {
-      const id = $(item).find('h2').find('a').eq(0).attr('href').match(regx)[1]
-      const title = $(item).find('h2').find('a').eq(0).text().trim().split(':')
-      const date = $(item).find('p').text().trim().match(/(.{4})-(.{2})-(.{2})/)[0]
-      // const index = $(item).find('h2').find('a').eq(1).text().trim().match(/第(.*?)期/)[1]
-      return {
-        // index,
-        id,
-        title,
-        date,
-      }
-    })
+    const result = $('#menu-list li')
+      .toArray()
+      .map((item: any) => {
+        const id = $(item)
+          .find('h2')
+          .find('a')
+          .eq(0)
+          .attr('href')
+          .match(regx)[1]
+        const title = $(item)
+          .find('h2')
+          .find('a')
+          .eq(0)
+          .text()
+          .trim()
+          .split(':')
+        const date = $(item)
+          .find('p')
+          .text()
+          .trim()
+          .match(/(.{4})-(.{2})-(.{2})/)[0]
+        // const index = $(item).find('h2').find('a').eq(1).text().trim().match(/第(.*?)期/)[1]
+        return {
+          // index,
+          id,
+          title,
+          date,
+        }
+      })
     res.send({
       code: 200,
       data: {
@@ -224,7 +349,9 @@ module.exports = {
    */
   async ['/renaibook_detail']({ req, res, request, cheerio }) {
     const { id } = req.query
-    const html = await request.send(`http://www.kekenet.com/chuzhong/${id}.shtml`)
+    const html = await request.send(
+      `http://www.kekenet.com/chuzhong/${id}.shtml`
+    )
     const $ = cheerio.load(html)
     const title = $('#nrtitle').text().trim()
     const content = $('#article .info-qh').html()
@@ -235,8 +362,13 @@ module.exports = {
       code: 200,
       data: {
         title,
-        content: StringUtil.formatUnicode(content).replace('译文属可可英语原创，未经允许，不得转载。', '').replace('译文属可可原创，仅供学习交流使用，未经许可请勿转载。', '').replace(/来源:可可英语/g, '').replace(/可可/g, 'Daily每日').replace(/&apos/g, '’'),
-        audio
+        content: StringUtil.formatUnicode(content)
+          .replace('译文属可可英语原创，未经允许，不得转载。', '')
+          .replace('译文属可可原创，仅供学习交流使用，未经许可请勿转载。', '')
+          .replace(/来源:可可英语/g, '')
+          .replace(/可可/g, 'Daily每日')
+          .replace(/&apos/g, '’'),
+        audio,
       },
     })
   },
